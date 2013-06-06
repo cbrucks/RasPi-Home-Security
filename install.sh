5ff03e57fd9e0e6ee89ed93bdf7ea5d2ff165226
root <root@chris-openstack.(none)>
2013-06-06 19:13:51 +0000

    Backup the previous version of the banner

--- install.sh
+++ install.sh
@@ -12,8 +12,13 @@ cinfo "Updating Packages"
 apt-get update -qq; apt-get upgrade -yqq;
 
 cinfo "Installing Custom Banner"
+# Backup current Banners
+mv /etc/issue /etc/issue.backup
+mv /etc/issue.net /etc/issue.net.backup
 # Install Custom Banner and activate it in the config file
-cp files/issue.net /etc/issue.net
-cp files/issue.net /etc/issue
+cp files/banner /etc/issue.net
+cp files/banner /etc/issue
 sed -ir "s/#Banner .*/Banner \/etc\/issue.net/g" /etc/ssh/sshd_config
+# TODO:Disable some of the post login banners
+# Restart the ssh server
 service ssh restart
