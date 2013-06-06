86258a1a1a18733dd9a253b9753b206269d50225
chri5759 <chris.brucks@rackspace.com>
2013-06-06 13:19:08 -0500

    Add lines to update local shell and restart ssh service

--- install.sh
+++ install.sh
@@ -5,5 +5,7 @@
 apt-get update; apt-get upgrade -yqq;
 
 # Install Custom Banner and activate it in the config file
+cp files/issue.net /etc/issue.net
 cp files/issue.net /etc/issue
 sed -ir "s/#Banner .*/Banner \/etc\/issue.net/g" /etc/ssh/sshd_config
+service ssh restart
