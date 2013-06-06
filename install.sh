af88c833efe00f3f3fc4d45c841158c35db9282c
chri5759 <chris.brucks@rackspace.com>
2013-06-06 13:01:39 -0500

    Add initial ssh banner customizations

---
+++ install.sh
@@ -0,0 +1,8 @@
+#! /bin/bash/
+# vim: 
+
+apt-get update; apt-get upgrade -yqq;
+
+# Install Custom Banner and activate it in the config file
+cp files/issue.net /etc/issue.net
+sed -r "s/#Banner .*/Banner \/etc\/issue.net/g" /etc/ssh/sshd_config
