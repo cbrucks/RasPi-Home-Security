9fde1801e0f7a26a49164d62104868d029cddf0d
chri5759 <chris.brucks@rackspace.com>
2013-06-06 13:22:56 -0500

    Add colored status messages

--- install.sh
+++ install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 # vim: 
 
+function cinfo() {
+	COLOR='\033[01;33m'	# bold yellow
+	RESET='\033[00;00m'	# normal white
+	MESSAGE=${@:-"${RESET}Error: No message passed"}
+	echo -e "${COLOR}${MESSAGE}${RESET}"
+}
 
+cinfo "Updating Packages"
 apt-get update -qq; apt-get upgrade -yqq;
 
+cinfo "Installing Custom Banner"
 # Install Custom Banner and activate it in the config file
 cp files/issue.net /etc/issue.net
 cp files/issue.net /etc/issue
