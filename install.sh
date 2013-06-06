d65c25058015d2fda9fe26cedb1151ff23bc2e86
chri5759 <chris.brucks@rackspace.com>
2013-06-06 13:11:31 -0500

    fix bug where new banner doesn't get activated

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ apt-get update; apt-get upgrade -yqq;
 
 # Install Custom Banner and activate it in the config file
 cp files/issue.net /etc/issue
-sed -r "s/#Banner .*/Banner \/etc\/issue.net/g" /etc/ssh/sshd_config
+sed -ir "s/#Banner .*/Banner \/etc\/issue.net/g" /etc/ssh/sshd_config
