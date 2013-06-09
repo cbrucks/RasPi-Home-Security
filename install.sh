cf2c121b07b9048e01dbc4c1b94aa335865d4834
Chris Brucks <cbrucks@gmail.com>
2013-06-09 02:44:08 +0000

    Add package installation step to install script

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ function c_header() {
 	echo "###########################################################"
 }
 function c_text() {
-	COLOR='\033[01;36m'	# bold cyan
+	COLOR='\033[01;00m'	# bold cyan
 	RESET='\033[00;00m'	# normal white
 	MESSAGE=${@:-"${RESET}Error: No message passed"}
-        echo -e "+--${COLOR}${MESSAGE}${RESET}"
+        echo -e "+${COLOR}${MESSAGE}${RESET}"
 }
 
 if [ "$(id -u)" != "0" ]; then 
@@ -23,6 +23,12 @@ fi
 c_header ${0##*/}
 
 cd $(dirname $0)
+
+c_text "Update package repos"
+sudo apt-get update -qq
+
+c_text "Install required packages"
+sudo apt-get install -yqq alsa-utils mpg321
 
 if [ -d init ]; then
   files=$( ls -1 init | grep -G ".sh$" )
