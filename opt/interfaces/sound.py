d4fa479b23b86742640e9e0d52395afc3c622e98
Chris Brucks <cbrucks@gmail.com>
2013-06-13 05:44:20 +0000

    modifications to sound.py

--- opt/interfaces/sound.py
+++ opt/interfaces/sound.py
@@ -1,17 +1,23 @@
 import os
 import ConfigParser
+import pygame.mixer
+from time import sleep
+from sys import exit
+import subprocess
 
 class Sound:
 
     def __init__(self,config):
-        self.button_sound=config.get("Sound","button")
-        self.armed_sound=config.get("Sound","armed")
-        self.alarm_sound=config.get("Sound","alarm")
-        self.warning_sound=config.get("Sound","warning")
+        BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+        self.button_sound  = BASE_DIR + "/" + config.get("Sound","button")
+        self.armed_sound   = BASE_DIR + "/" + config.get("Sound","armed")
+        self.alarm_sound   = BASE_DIR + "/" + config.get("Sound","alarm")
+        self.warning_sound = BASE_DIR + "/" + config.get("Sound","warning")
 
     def _play_sound(self, fname):
-        if ((fname.split('.'))[-1] == "mp3") and (os.path.isfile(fname)):
-            os.system("mpg321 -q " + fname)
+        if ((fname.split('.'))[-1] == "wav") and (os.path.isfile(fname)):
+            print fname
+            subprocess.Popen(["aplay", fname])
         else:
             raise Exception("File " + fname + " does not exist.")
 
@@ -28,8 +34,15 @@ class Sound:
         self._play_sound(self.warning_sound)
 
 if __name__ == "__main__":
-    sound = Sound()
+    conf = ConfigParser.RawConfigParser()
+    conf.readfp(open("../default.cfg"))
+    sound = Sound(conf)
+    print "playing"
     sound.play_warning_sound()
+    sleep(1)
     sound.play_button_sound()
+    sleep(1)
     sound.play_armed_sound()
+    sleep(1)
     sound.play_alarm_sound()
+    print "done"
--- opt/main.py
+++ opt/main.py
@@ -6,6 +6,11 @@ from interfaces.ip import IP
 from interfaces.email_conductor import Email_Conductor
 from interfaces.sms import SMS
 
+
+def func(string,sleep,sound):
+    sound.play_alarm_sound()
+    
+
 if __name__ == "__main__":
     # Read in the configuration file\
     user_config = 'conf.cfg'
@@ -24,9 +29,9 @@ if __name__ == "__main__":
     sound.play_warning_sound()
     sound.play_armed_sound()
     sound.play_alarm_sound()
-
-    email_conductor.send_emergency_email()
-    email_conductor.send_maintenence_email()
-
-    sms.send_emergency_sms()
-    sms.send_maintenence_sms()
+    sleep(1)
+#     email_conductor.send_emergency_email()
+#     email_conductor.send_maintenence_email()
+# 
+#     sms.send_emergency_sms()
+#     sms.send_maintenence_sms()
